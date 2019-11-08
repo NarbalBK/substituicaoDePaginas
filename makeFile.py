68cc4b60c1ec590159c15397a7a437bae03a624b
Péricles Narbal <periclesnarbal@MacBook-Pro-de-Pericles.local>
2019-11-08 17:34:02 -0300

    codigo para gerar entradas

---
+++ makeFile.py
@@ -0,0 +1,15 @@
+from random import randint
+
+def makeFile(filename, base, size):
+    fileEntry = open(filename + ".txt","w+")
+
+    for _ in range(size):
+        num = randint(1, base)
+        wordSeed = randint(0, 1)
+        word = 'R' if wordSeed==0 else 'W'
+        fileEntry.write("%d%s-" % (num, word))
+
+    fileEntry.close
+
+
+makeFile('teste', 7, 20)
